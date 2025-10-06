e1242974143d5c93c55e71691810a3344f9b23cd
matt secondi <matt_secondi@hotmail.com>
2025-10-06 12:30:09 -0400

    simple touch up

--- utils/types.ts
+++ utils/types.ts
@@ -3,9 +3,8 @@ export interface SubjectScore {
     score: number;
 }; //[]    if you put that at the end of this type alias, it's saying this type is an array of objects containing thos key/value pairs
 
-//this also accomplishes the same thing ^
+//this ^ also accomplishes the same thing
 // export type SubjectScores = SubjectScore[];
-
 
 //INTERFACE
 export interface StudentScore {
